fcd2d7db1dcb9303ce86d66a8c896cdff6c72937
naresh mahto <mahtonaresh059@gmail.com>
2023-09-13 22:54:37 +0530

    new third commit

--- Class And Object/ClassAndObject/src/temp/StudentUse2.java
+++ Class And Object/ClassAndObject/src/temp/StudentUse2.java
@@ -1,5 +1,13 @@
 package temp;
 
+import classandobject.Student;
+
 public class StudentUse2 {
+	
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println(s1.name);
+				
+	}
 
 }
